326e1176189540dd5319675fca18a78c8c0bbef6
dkmullen <dkmullen@gmail.com>
2016-03-21 18:47:24 -0400

    feat: Complete the writing of all tests

--- feedreader.js
+++ feedreader.js
@@ -80,18 +80,53 @@ $(function() {
 	});
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+	describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+		beforeEach(function(done) {
+			loadFeed(0, done);
+		});
+		it('should contain at least one entry', function(done) {
+			//look for the class 'entry' in the DOM feed container, check to
+			//see that it isn't empty (ie, of zero length)
+			expect($('.entry').length).toBeGreaterThan(0);
+			//console.log($('.entry').length);
+			done();
+		});
 
-    /* TODO: Write a new test suite named "New Feed Selection"
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+	});	 
+    /* TODO: Write a new test suite named "New Feed Selection" */
+	describe('New Feed Selection', function() {
+		/* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+		var feedTitle = $('.header-title').html(); //header-title == 'Feeds', from html
+		
+		beforeEach(function(done) {
+			/* load feed #1, CSS Tricks in this case. When this loads, header-title
+			 * changes to 'CSS Tricks' and then the comparison at the bottom is run.
+			 */
+			loadFeed(1, done);
+		});
+		
+		/* This simply changes the feed back to the default, 'Udacity Blog 
+		 * after the test runs */
+		afterEach(function(done) {
+			loadFeed(0, done);
+		});
+		
+		/* So, the test simply makes sure that header-title changes from the 
+		 * default, which is set in the html, to something else, presumably
+		 * from a feed */
+		it('should change when new feed loads', function(done) {
+			expect($('.header-title').html()).not.toBe(feedTitle);
+			done();
+		});
+		
+	});	 
 }());
